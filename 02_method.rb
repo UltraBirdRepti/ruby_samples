f8e061574efbbba523938ce9f521402d382fa2da
UltraBirdRepti <ultrabirdrepti@gmail.com>
2017-11-09 06:17:04 +0900

    add comment

--- 02_method.rb
+++ 02_method.rb
@@ -1,10 +1,18 @@
 # methodに関してです。
 # 参考:パーフェクトRuby P.42 2章Rubyの基礎
+
+# methodの定義はdefを使用する
+# hello()メソッドは引数としてnamesを使用する
 def hello(names)
+  # 引数を受け取って、each()で引回す。
   names.each do |name|
+    # puts()で結果をアウトプット
+    # upcase()は、Stringに対して大文字にする
     puts "HELLO, #{name.upcase}"
   end
 end
 
+# 配列を定義
 rubies = ['MRI', 'jruby', 'rubinius']
+# メソッド呼び出し
 hello(rubies)

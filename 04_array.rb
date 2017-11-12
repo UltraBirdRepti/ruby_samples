170a2897b1f5cc1e894667ea2ad3430706ea11ef
UltraBirdRepti <ultrabirdrepti@gmail.com>
2017-11-12 10:08:27 +0900

    edit 04_array.rb

--- 04_array.rb
+++ 04_array.rb
@@ -9,3 +9,15 @@ array[0] = 'first'
 array << 'second'
 
 p array  # ['first', 'second']
+
+# 要素の引き算
+p ['a', 'b', 'c'] - ['c']  # ['a', 'b']
+
+# 要素の削除
+delete_array = ['a', 'b', 'c']
+# 指定した要素を削除
+p delete_array.delete('b') # => 'b'
+# 指定した要素順にある要素を削除
+p delete_array.delete_at(0) # => 'a'
+
+p delete_array

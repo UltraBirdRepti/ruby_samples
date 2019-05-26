460d3d7af88fb57b3e57bad454f5360a50bc7390
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-05-26 17:05:23 +0900

    add zundoko file

---
+++ 08_zundoko.rb
@@ -0,0 +1,18 @@
+zun = 'ズン'
+doko = 'ドコ'
+
+array = []
+
+loop do
+  array << [zun, doko].sample
+  p array[-1]
+
+  if !array[-5..-1].nil?
+    if array[-5..-1].join == 'ズンズンズンズンドコ'
+      p 'KI・YO・SHI'
+      break;
+    else
+      array.shift
+    end
+  end
+end
